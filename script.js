fcfb9171f2d8d214df94c1f1cba7a4e02cc85a5a
nikaTqw <161809588+nikaTqw@users.noreply.github.com>
2025-10-21 15:52:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// forms
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
